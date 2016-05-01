35d9bdc7d04ea11fca4eec273056b3b6c20aac3a
KimCoding <jeokrang@hanmail.net>
2016-05-01 22:03:57 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /src\/.+.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            }
+        ]
+    }
+};
