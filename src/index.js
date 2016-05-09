89bbbfd1243a02c312838b31832cbf4d2b8f6323
KimCoding <jeokrang@gmail.com>
2016-05-09 18:17:42 +0900

    fix: arrange code
    - remove unneccessary files
    - move App.js from /components to /containers

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './reducers'
-import App from './components/App'
+import App from './containers/App'
 
 let store = createStore(todoApp);
 
